92ed5b8e540389c361adc141a7e360aa9fdf18ae
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-03-01 16:35:49 +0700

    task: add coding challenge p1 caesar cipher

---
+++ 7_String – Advance Function – Pointer – Method – Struct and Interface/praktikum/p2-caesar-cipher.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func cipherEncrypt(offset int, input string) string {
+	var result string
+	var initialCharacter int = int('a') - 1
+	var lastCharacter int = int('z')
+	offset = offset % initialCharacter
+	fmt.Println(string(rune(offset + 96)))
+
+	for _, character := range input {
+		var encryptedCharacterAscii int = int(character) + offset
+
+		if encryptedCharacterAscii > lastCharacter {
+			result += string(rune(((int(character) + offset) - lastCharacter) + initialCharacter))
+		} else {
+			result += string(rune((int(character) + offset)))
+		}
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(cipherEncrypt(10, "alterra"))
+}
