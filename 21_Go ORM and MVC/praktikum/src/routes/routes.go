b26258c1ca08c5841ae779d293e183a73281e4c5
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-03-31 16:46:44 +0700

    feat(c21): add full CRUD purpose routes

--- 21_Go ORM and MVC/praktikum/src/routes/routes.go
+++ 21_Go ORM and MVC/praktikum/src/routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"gorm-mvc-api/src/controllers/books"
 	"gorm-mvc-api/src/controllers/users"
 
 	"github.com/labstack/echo/v4"
@@ -17,6 +18,11 @@ func New() *echo.Echo {
 	route.DELETE("/users/:id", users.DeleteUser)
 
 	// books
+	route.GET("/books", books.GetAllBooks)
+	route.POST("/books", books.CreateBook)
+	route.GET("/books/:id", books.GetBook)
+	route.PUT("/books/:id", books.UpdateBook)
+	route.DELETE("/books/:id", books.DeleteBook)
 
 	return route
 }
